a7bdfe195210974eb3f5b37145cfcc7bfe72feff
Valeriia Oliinyk <olijnykvaleria@gmail.com>
2020-09-15 19:51:37 +0300

    added sections and animation

---
+++ src/js/animation.js
@@ -0,0 +1,17 @@
+AOS.init({
+  disable: false,
+  startEvent: 'DOMContentLoaded',
+  initClassName: 'aos-init',
+  animatedClassName: 'aos-animate',
+  useClassNames: false,
+  disableMutationObserver: false,
+  debounceDelay: 50,
+  throttleDelay: 99,
+  offset: 100,
+  delay: 400,
+  duration: 1200,
+  easing: 'ease',
+  once: false,
+  mirror: false,
+  anchorPlacement: 'top-bottom',
+});
